9cff7cae20d2c5a3716ce2a2e2bd97948f084bf1
Vyacheslav <dikey677@gmail.com>
2022-10-23 21:34:55 +0300

    add tablet

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,36 +1,91 @@
-// import { ReactComponent as EllipseСoral } from "../image/SVG/EllipseCoral.svg";
 import { ReactComponent as Wallet } from "../image/SVG/Wallet.svg";
-import { useFormik } from 'formik';
+import { useFormik } from "formik";
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = "Required";
+  } else if (1 > values.name.length > 12) {
+    errors.name = "Must be 15 characters or less";
+  }
+
+  if (!values.pass) {
+    errors.pass = "Required";
+  } else if (values.pass.length > 20) {
+    errors.pass = "Must be 20 characters or less";
+  }
+
+  if (!values.confPass) {
+    errors.confPass = "Required";
+  } else if (values.confPass.length > 20) {
+    errors.confPass = "Must be 20 characters or less";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  return errors;
+};
 
 const RegistrationForm = () => {
-    const formik = useFormik({
-     initialValues: {
-       firstName: '',
-       lastName: '',
-       email: '',
-     },
-     onSubmit: values => {
-       alert(JSON.stringify(values, null, 2));
-     },
-    });
-    
-    return (
-        <div className="registration-form">
-                {/* <EllipseСoral className="ellipseImg2"/> */}
-                <form className="registration-form__fields">
-                    <ul className="registration-form__list">
-                        <li className="registration-form__item"><Wallet/></li>
-                        <li className="registration-form__item"><h1 className="registration-form__wallet">Wallet</h1></li>
-                    </ul>
-                    
-                    <input type="text" placeholder="E-mail" />
-                    <input type="text" placeholder="Пароль" />
-                    <input type="text" placeholder="Подтвердите пароль" />
-                    <input type="text" placeholder="Ваше имя" />
-                    <button type="submit" className="registration-form__button">ВХОД</button>
-                    <button type="submit">РЕГИСТРАЦИЯ</button>
-                </form>
-        </div>)
-}
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      pass: "",
+      confPass: "",
+      name: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
 
-export default RegistrationForm
+  return (
+    <div className="registrationForm">
+      <form className="registrationForm__form">
+        <ul className="registrationForm__list">
+          <li className="registrationForm__item">
+            <Wallet />
+          </li>
+          <li className="registrationForm__item">
+            <h1 className="registrationForm__wallet">Wallet</h1>
+          </li>
+        </ul>
+
+        <input
+          className="registrationForm__inputEmail registrationForm__input--all"
+          type="email"
+          placeholder="E-mail"
+        />
+        <input
+          className="registrationForm__inputPass registrationForm__input--all"
+          type="password"
+          placeholder="Пароль"
+        />
+        <input
+          className="registrationForm__inputConfPass registrationForm__input--all"
+          type="password"
+          placeholder="Подтвердите пароль"
+        />
+        <input
+          className="registrationForm__inputName registrationForm__input--all"
+          type="name"
+          placeholder="Ваше имя"
+        />
+
+        <button type="submit" className="registrationForm__button">
+          Регистрация
+        </button>
+        <button type="button" className="registrationForm__button">
+          Вход
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default RegistrationForm;
--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -3,19 +3,18 @@ import { ReactComponent as EllipsePurple } from "../image/SVG/EllipsePurple.svg"
 import { ReactComponent as GroupSignUp } from "../image/SVG/GroupSignUp.svg";
 import { ReactComponent as EllipseСoral } from "../image/SVG/EllipseCoral.svg";
 
-
 const RegistrationPage = () => {
-    return (
-        <div className="registration-page">
-            <div className="registration-page__wallet">
-                <GroupSignUp className="groupImg"/>
-                <EllipsePurple className="eclipseImg" />
-                <EllipseСoral className="ellipseImg2"/>
-                <title className="title">Finance App</title>
-            </div>
-            <RegistrationForm />
-        </div>
+  return (
+    <div className="registrationPage">
+      <div className="registrationPage__container">
+        <GroupSignUp className="registrationPage__imgGroup" />
+        <EllipsePurple className="registrationPage__imgEllipsePurple" />
+        <EllipseСoral className="registrationPage__imgEllipseCoral" />
+        <title className="registrationPage__title">Finance App</title>
+      </div>
+      <RegistrationForm />
+    </div>
+  );
+};
 
-    )
-}
-export default RegistrationPage
+export default RegistrationPage;
