bae347fd20e859e216a25a7c4d8c3fc1f493e89d
Vyacheslav <dikey677@gmail.com>
2022-11-26 18:33:30 +0300

    Завершил стилизацию таблицы Currency

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,31 +1,143 @@
-const Currency = () => {
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import { ReactComponent as Vector } from "../image/SVG/MobileVector.svg";
+
+const Table = ({ columns, data }) => {
+  const { getTableProps, headerGroups, rows, prepareRow, getTableBodyProps } =
+    useTable({
+      columns,
+      data,
+    });
+
   return (
-    <table className="table">
-      <thead className="table-head">
-        <tr className="table-header">
-          <td className="table-title">Валюта</td>
-          <td className="table-title">Покупка</td>
-          <td className="table-title">Продажа</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>USD</td>
-          <td>27.55</td>
-          <td>27.65</td>
-        </tr>
-        <tr>
-          <td>EUR</td>
-          <td>30.00</td>
-          <td>30.10</td>
-        </tr>
-        <tr>
-          <td>RUB</td>
-          <td>00.00</td>
-          <td>00.00</td>
-        </tr>
-      </tbody>
-    </table>
+    <>
+      <table
+        {...getTableProps()}
+        style={{
+          margin: "30px auto",
+          width: "280px",
+          height: "174px",
+          background: "#4A56E2",
+          borderCollapse: "collapse",
+          borderRadius: "30px",
+          position: "relative",
+        }}
+      >
+        <thead
+          style={{
+            background: "#6e78e8",
+            height: "50px",
+            borderRadius: "30px 30px 0 0",
+            display: "table-cell",
+          }}
+        >
+          {headerGroups.map((headerGroup) => (
+            <tr
+              {...headerGroup.getHeaderGroupProps()}
+              style={{
+                display: "flex",
+                width: "73px",
+                height: "27px",
+              }}
+            >
+              {headerGroup.headers.map((column) => (
+                <th
+                  {...column.getHeaderProps()}
+                  style={{
+                    fontStyle: "normal",
+                    fontWeight: "700",
+                    fontSize: "16.5px",
+                    lineHeight: "27px",
+                    textAlign: "center",
+                    color: "#FFFFFF",
+                    marginLeft: "20px",
+                  }}
+                >
+                  {column.render("Header")}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody
+          {...getTableBodyProps()}
+          style={{
+            display: "table-row",
+          }}
+        >
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr
+                {...row.getRowProps()}
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  margin: "10px 20px",
+                  alignItems: "center",
+                }}
+              >
+                {row.cells.map((cell) => {
+                  return (
+                    <td
+                      {...cell.getCellProps()}
+                      style={{
+                        fontStyle: "normal",
+                        fontWeight: "400",
+                        fontSize: "16px",
+                        lineHeight: "24px",
+                        color: "#FFFFFF",
+                      }}
+                    >
+                      {cell.render("Cell")}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Vector
+        style={{
+          position: "absolute",
+          left: "19px",
+          top: "226px",
+        }}
+      />
+    </>
   );
 };
+
+const Currency = () => {
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Валюта",
+        accessor: "currency",
+      },
+      {
+        Header: "Покупка",
+        accessor: "buy",
+      },
+      {
+        Header: "Продажа",
+        accessor: "sell",
+      },
+    ],
+    []
+  );
+
+  const data = useMemo(
+    () => [
+      { currency: "USD", buy: "27.55", sell: "27.65" },
+      { currency: "EUR", buy: "30.55", sell: "31.65" },
+      { currency: "RUB", buy: "29.05", sell: "33.75" },
+    ],
+    []
+  );
+
+  return <Table columns={columns} data={data} />;
+};
+
 export default Currency;
