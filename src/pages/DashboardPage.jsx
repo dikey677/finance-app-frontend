1a5eee672ab03fc298cd732e3b035c8ed23056d6
Vyacheslav <dikey677@gmail.com>
2022-11-26 19:17:42 +0300

    Добавлен header на странице пользователя

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,24 +1,41 @@
+import { ReactComponent as Wallet } from "../image/SVG/Wallet.svg";
 import { ReactComponent as Currency } from "../image/SVG/Currency.svg";
 import { ReactComponent as Statistic } from "../image/SVG/Statistic.svg";
+import { ReactComponent as Logout } from "../image/SVG/Exit.svg";
 import { ReactComponent as Home } from "../image/SVG/Home.svg";
 import CurrencyComponent from "../components/Currency";
 
 const DashboardPage = () => {
   return (
-    <div className="dashboard">
-      <ul className="dashboard-list">
-        <li className="dashboard-list__item">
-          <button type="button" className="dashboard-list__button ">
+    <div className="db">
+      <div className="db-header">
+        <div className="db-header__wallet">
+          <span className="db-geader__logo">
+            <Wallet />
+          </span>
+          <h2 className="db-header__title">Wallet</h2>
+        </div>
+        <div className="db-header__user">
+          <p className="db-header__current-user">Имя</p>
+          <button type="submit" className="db-header__logout">
+            <Logout />
+          </button>
+        </div>
+      </div>
+
+      <ul className="db-list">
+        <li className="db-list__item">
+          <button type="button" className="db-list__button ">
             <Home />
           </button>
         </li>
-        <li className="dashboard-list__item">
-          <button type="button" className="dashboard-list__button">
+        <li className="db-list__item">
+          <button type="button" className="db-list__button">
             <Statistic />
           </button>
         </li>
-        <li className="dashboard-list__item">
-          <button type="button" className="dashboard-list__button">
+        <li className="db-list__item">
+          <button type="button" className="db-list__button">
             <Currency />
           </button>
         </li>
