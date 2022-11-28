9efb35062129b265ad682ca27c50b48305f82b14
Vyacheslav <dikey677@gmail.com>
2022-11-28 23:50:41 +0300

    настроил стилизацию таблицы HomeTab

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -21,38 +21,97 @@ const Table = () => {
                 height: "282px",
                 borderRadius: "10px",
                 display: "block",
+                borderCollapse: "collapse",
               }}
             >
-              <tbody
-                style={{
-                  display: "block",
-                }}
-              >
+              <tbody>
                 <tr
                   style={{
+                    display: "flex",
                     borderBottom: "1px solid #DCDCDF",
+                    width: "280px",
+                    height: "47px",
+                    justifyContent: "space-between",
+                    padding: "12px 20px",
                   }}
                 >
                   <th>Дата</th>
-                  <td>04.01.19</td>
+                  <td style={{}}>04.01.19</td>
                 </tr>
-                <tr>
+                <tr
+                  style={{
+                    display: "flex",
+                    borderBottom: "1px solid #DCDCDF",
+                    justifyContent: "space-between",
+                    width: "280px",
+                    height: "47px",
+                    padding: "12px 20px",
+                  }}
+                >
                   <th>Тип</th>
                   <td>-</td>
                 </tr>
-                <tr>
+                <tr
+                  style={{
+                    display: "flex",
+                    borderBottom: "1px solid #DCDCDF",
+                    justifyContent: "space-between",
+                    width: "280px",
+                    height: "47px",
+                    padding: "12px 20px",
+                  }}
+                >
                   <th>Категория</th>
                   <td>Разное</td>
                 </tr>
-                <tr>
+                <tr
+                  style={{
+                    display: "flex",
+                    borderBottom: "1px solid #DCDCDF",
+                    justifyContent: "space-between",
+                    width: "280px",
+                    height: "47px",
+                    padding: "12px 20px",
+                  }}
+                >
                   <th>Комментарий</th>
                   <td>Подарок жене</td>
                 </tr>
-                <tr>
+                <tr
+                  style={{
+                    display: "flex",
+                    borderBottom: "1px solid #DCDCDF",
+                    justifyContent: "space-between",
+                    width: "280px",
+                    height: "47px",
+                    padding: "12px 20px",
+                  }}
+                >
                   <th>Сумма</th>
-                  <td>300.00</td>
+                  <td>
+                    <span
+                      style={{
+                        // fontFamily: "Circe",
+                        fontStyle: "normal",
+                        fontWeight: 700,
+                        fontSize: "16px",
+                        lineHeight: "24px",
+                        color: "#FF6596",
+                      }}
+                    >
+                      300.00
+                    </span>
+                  </td>
                 </tr>
-                <tr>
+                <tr
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    width: "280px",
+                    height: "47px",
+                    padding: "12px 20px",
+                  }}
+                >
                   <th>Баланс</th>
                   <td>6900.00</td>
                 </tr>
